ea0e7158c3827066a1ae02c1607eb6b54f044516
Taha Kaplan <taha0kaplan@gmail.com>
2024-11-06 17:03:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ehliyet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age : ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Education Status");
+            var education = Console.ReadLine();
+            var decision = (age >= 18 && education == "High School" || education == "University") ? "You Can Get a Driving License" : "You Can't Get a Driving License";
+            Console.WriteLine(decision);
+            Console.ReadLine();
+
+
+        }
+
+
+
+    }
+}
